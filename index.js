504c79b5ab6e1d2da9d68f1fe5aa1c925aefebf5
moohh <mo051191@yahoo.com>
2019-08-11 20:16:26 -0400

    .

--- index.js
+++ index.js
@@ -11,51 +11,8 @@ let health = {
     msg: ''
 };
 
-// q setup
-var qlock = 0;
-var qc = 0;
-let q = [];
-
 // particlein
 let ptin = 0;
-
-// interval tracker
-let intervals = [];
-
-// create consumer
-setInterval(() => {
-    // retrieve lock
-    while (true) {
-        if (qlock == 0) {
-            qlock++;
-            break;
-        } else if (qlock == 1) {
-            continue;
-        } else {
-            throw 'deadlock';
-        }
-    }
-
-    let qe = Array.from(q);
-    qc += qe.length;
-    q = [];
-    // release lock
-    qlock--;
-
-    // process q items
-    if (qe.length > 0) {
-        for (var i = qe.shift(); i != undefined; i = qe.shift()) {
-            axios.post(i.to, { particle: i.particle })
-                .catch((err) => {
-                    // need to log this
-                    console.log(err);
-                });
-        }
-    }
-
-    intervals.push({ time: moment.now(), qc });
-}, 1000);
-
 
 let app = e();
 
@@ -67,17 +24,8 @@ app.get('/health', (req, res) => {
     res.json(health);
 });
 
-app.get('/status/q', (req, res) => {
-    res.json(q.length);
-});
-
 app.get('/status/ptin', (req, res) => {
     res.json(ptin);
-});
-
-app.get('/status/latency', (req, res) => {
-    let int = intervals.splice(intervals.length-2);
-    res.json( `${(int[1].qc-int[0].qc) / ((int[1].time - int[0].time) / 1000.0)} particles per second` );
 });
 
 // posts
@@ -87,29 +35,15 @@ app.post('/ping', (req, res) => {
         return;
     }
 
-    let { particle } = req.body;
-    if (!particle) {
+    let { data } = req.body;
+    if (!data) {
         res.status(400).send(new Error('missing particle object'));
         return;
     }
 
     ptin++;
 
-    // retrieve lock
-    while (true) {
-        if (qlock == 0) {
-            qlock++;
-            break;
-        }
-    }
-    q.push({
-        to: particle.back,
-        "particle": particle
-    });
-    // release lock
-    qlock--;
-
-    res.send(`particle recieved [${particle.correlationId}]`);
+    res.json(data);
 });
 
 // kill and rez
@@ -117,14 +51,14 @@ app.post('/poison', (req, res) => {
     health.status = 'poisoned';
     health.msg = 'app poisoned';
     
-    res.send('app poisoned');
+    res.send('app.poison');
 });
 
 app.post('/replenish', (req, res) => {
     health.status = 'listening';
     health.msg = `listening on port [${configs.port}]`;
     
-    res.send('app replenished');
+    res.send('app.replenish');
 })
 
 app.listen(configs.port, () => { 
